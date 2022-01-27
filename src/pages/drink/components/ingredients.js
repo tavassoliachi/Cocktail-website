cbecc87e1b91797a72e463b925f18a2929fd1ae6
tavassoliachi <tavassoliachi@gmail.com>
2022-01-27 12:03:39 +0400

    ...

--- src/pages/drink/components/ingredients.js
+++ src/pages/drink/components/ingredients.js
@@ -1,71 +1,88 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchIngredient } from '../../../redux/actions/fetchActions';
-import { useSelector } from 'react-redux';
-import styles from "../styles.module.css"
-import { styled } from '@mui/material/styles';
-import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchIngredient } from "../../../redux/actions/fetchActions";
+import { useSelector } from "react-redux";
+import styles from "../styles.module.css";
+import { styled } from "@mui/material/styles";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
 
-export default function Ingredients({drink}) {
-      useEffect(()=>{
-         ingredientDetails()
-   },[])
-   const dispatch = useDispatch()
-   const data = useSelector((el)=>{return el.ingredientsData.data})
+export default function Ingredients({ drink }) {
+  useEffect(() => {
+    ingredientDetails();
+  }, []);
+  const dispatch = useDispatch();
+  const data = useSelector((el) => {
+    return el.ingredientsData.data;
+  });
 
-    const HtmlTooltip = styled(({ className, ...props }) => (
-        <Tooltip {...props} classes={{ popper: className }} />
-      ))(({ theme }) => ({
-        [`& .${tooltipClasses.tooltip}`]: {
-          backgroundColor: '#f5f5f9',
-          color: 'rgba(0, 0, 0, 0.87)',
-          maxWidth: 320,
-          fontSize: theme.typography.pxToRem(12),
-          border: '1px solid #dadde9',
-        },
-      }));
+  const HtmlTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: "#f5f5f9",
+      color: "rgba(0, 0, 0, 0.87)",
+      maxWidth: 320,
+      fontSize: theme.typography.pxToRem(12),
+      border: "1px solid #dadde9",
+    },
+  }));
 
+  const ingredientDetails = () => {
+    var ingredients = [];
+    const keys = drink  ? Object.keys(drink).filter(
+          (el) => el.includes("strIngredient") && drink[el]
+        ) : [];
+    keys?.forEach((el) => {
+      return ingredients.push(drink[el]);
+    });
+    ingredients.length && dispatch(fetchIngredient(ingredients));
+  };
 
-
-    const ingredientDetails = () => {
-        var ingredients = []
-        const keys = drink  ? Object.keys(drink).filter((el)=>el.includes("strIngredient") && drink[el]) : []
-        keys?.forEach((el)=>{return ingredients.push(drink[el])})
-        ingredients.length && dispatch(fetchIngredient(ingredients))
+  const handleTitle = (index) => {
+    var text =  data[index]?.strDescription || "Desctiption for this ingredient was not found";
+    var seeMore = false;
+    const words = text?.split(" ");
+    if (words.length > 100) {
+      seeMore = true;
+      words.length = 100;
+      text = words.join(" ") + "...";
     }
-
-    const handleTitle = (index) => {
-         var text = data[index]?.strDescription || "Desctiption for this ingredient was not found"
-         var seeMore = false
-         const words = text?.split(' ')
-         if(words.length > 100){
-               seeMore = true
-               words.length=100
-               text=words.join(" ") + '...'
-         }
-         const handleSeeMore = () => {
-            document.getElementById(`tooltip-${index}`).innerHTML=`${data[index].strDescription}`
-         }
-         return [text,seeMore && <div className={styles.seeMore} onClick={()=>handleSeeMore()}>See more</div>]
-    }
+    const handleSeeMore = () => {
+      document.getElementById(`tooltip-${index}`).innerHTML = `${data[index].strDescription}`;
+    };
+    return [
+      text,
+      seeMore && (
+        <div className={styles.seeMore} onClick={() => handleSeeMore()}>
+          See more
+        </div>
+      ),
+    ];
+  };
 
   return (
+    <div>
+      {drink && data && (
         <div>
-             {drink && data && <div>
-                  <div className={styles.stepsTitle}>Ingredients</div>
-                  {
-                  Object.keys(drink).filter((el)=>el.includes("strIngredient") && drink[el]).map((el,index)=>
-                     {
-                           return <HtmlTooltip placement="left" key={el} title={<p id={`tooltip-${index}`}>{handleTitle(index)}</p>}>
-                                       <h1 className={styles.steps} >{drink[el]} - {drink[`strMeasure${index+1}`]}</h1>
-                                 </HtmlTooltip>
-
-                     })
-   
-                  }
-               </div>}
-
+          <div className={styles.stepsTitle}>Ingredients</div>
+          {Object.keys(drink)
+            .filter((el) => el.includes("strIngredient") && drink[el])
+            .map((el, index) => {
+              return (
+                <HtmlTooltip
+                  placement="left"
+                  key={el}
+                  title={<p id={`tooltip-${index}`}>{handleTitle(index)}</p>}
+                >
+                  <h1 className={styles.steps}>
+                    {drink[el]} - {drink[`strMeasure${index + 1}`]}
+                  </h1>
+                </HtmlTooltip>
+              );
+            })}
         </div>
+      )}
+    </div>
   );
 }
--- src/pages/drink/index.jsx
+++ src/pages/drink/index.jsx
@@ -30,7 +30,7 @@ export default function Drink() {
   return <div className={styles.cont}>
                 {loading ? <div style={{display:"flex",justifyContent:"center"}}><CircularProgress/></div> : 
                 drink ?
-                <>
+                <div className={styles.drinkCont}>
 
                             <h1 className={styles.drinkName}>{drink?.strDrink}</h1>
                         <div className={styles.subDetails}>
@@ -38,8 +38,8 @@ export default function Drink() {
                             <p>Updated 01/2/22</p>
                         </div>
                     <div className={styles.details}>
-                        <img src={`${drink?.strDrinkThumb}`} style={{width:'40%'}} alt=''/>
-                        <div style={{display:'flex',flexDirection:"column",width:"100%",marginLeft:"40px",justifyContent:"flex-start"}}>
+                        <img src={`${drink?.strDrinkThumb}`} style={{width:'40%',minWidth:"300px"}} alt=''/>
+                        <div className={styles.makingDetails} style={{display:'flex',flexDirection:"column",width:"100%",marginLeft:"40px",justifyContent:"flex-start"}}>
                             {drink && <Ingredients drink={drink}/>}
 
                             <div>
@@ -54,7 +54,7 @@ export default function Drink() {
                             <h1 className={styles.steps} style={{width:'60%'}}>{drink[lang]}</h1>
                             </div>
                         </div>
-                </div></>
+                </div></div>
                 :
                 <div style={{width:"100%",display:"flex",justifyContent:"center",flexDirection:"column"}}>
                     <h1 style={{width:'100%',textAlign:"center",fontWeight:"600"}}>{`Drink with id ${id} was not found`}</h1>
