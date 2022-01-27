77830181bbab0f540bc13a03c8f4dd3550a0898d
tavassoliachi <tavassoliachi@gmail.com>
2022-01-27 12:03:34 +0400

    ...

--- src/redux/consts/reducerType.js
+++ src/redux/consts/reducerType.js
@@ -1,31 +1,31 @@
 export const alcoholic = {
-    request:"Request_alcoholic",
-    success:"Success_alcoholic",
-    fail:"Fail_alcoholic"
-}
+  request: "Request_alcoholic",
+  success: "Success_alcoholic",
+  fail: "Fail_alcoholic",
+};
 export const nonAlcoholic = {
-    request:"Request_nonAlcoholic",
-    success:"Success_nonAlcoholic",
-    fail:"Fail_nonAlcoholic"
-}
+  request: "Request_nonAlcoholic",
+  success: "Success_nonAlcoholic",
+  fail: "Fail_nonAlcoholic",
+};
 export const singleDrink = {
-    request:"Request_singleDrink",
-    success:"Success_singleDrink",
-    fail:"Fail_singleDrink",
-    erase:"Erase_singleDrink"
-}
+  request: "Request_singleDrink",
+  success: "Success_singleDrink",
+  fail: "Fail_singleDrink",
+  erase: "Erase_singleDrink",
+};
 export const randomDrink = {
-    request:"Request_randomDrink",
-    success:"Success_randomDrink",
-    fail:"Fail_randomDrink"
-}
+  request: "Request_randomDrink",
+  success: "Success_randomDrink",
+  fail: "Fail_randomDrink",
+};
 export const search = {
-    request:"Request_searchDrink",
-    success:"Success_searchDrink",
-    fail:"Fail_searchDrink"
-}
+  request: "Request_searchDrink",
+  success: "Success_searchDrink",
+  fail: "Fail_searchDrink",
+};
 export const ingredient = {
-    request:"Request_ingredient",
-    success:"Success_ingredient",
-    fail:"Fail_ingredient"
-}
+  request: "Request_ingredient",
+  success: "Success_ingredient",
+  fail: "Fail_ingredient",
+};
