b96ba6221d552f1969edc6409ede382fa1c05213
tavassoliachi <tavassoliachi@gmail.com>
2022-01-27 10:30:34 +0400

    added tooltip "see more" functionality

--- src/pages/drink/components/ingredients.js
+++ src/pages/drink/components/ingredients.js
@@ -6,9 +6,14 @@ import { useSelector } from 'react-redux';
 import styles from "../styles.module.css"
 import { styled } from '@mui/material/styles';
 import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
-import { Link } from 'react-router-dom';
 
 export default function Ingredients({drink}) {
+      useEffect(()=>{
+         ingredientDetails()
+   },[])
+   const dispatch = useDispatch()
+   const data = useSelector((el)=>{return el.ingredientsData.data})
+
     const HtmlTooltip = styled(({ className, ...props }) => (
         <Tooltip {...props} classes={{ popper: className }} />
       ))(({ theme }) => ({
@@ -20,30 +25,31 @@ export default function Ingredients({drink}) {
           border: '1px solid #dadde9',
         },
       }));
-    useEffect(()=>{
-        ingredientDetails()
-    },[])
-    const dispatch = useDispatch()
+
+
+
     const ingredientDetails = () => {
         var ingredients = []
         const keys = drink  ? Object.keys(drink).filter((el)=>el.includes("strIngredient") && drink[el]) : []
         keys?.forEach((el)=>{return ingredients.push(drink[el])})
         ingredients.length && dispatch(fetchIngredient(ingredients))
     }
-    const data = useSelector((el)=>{return el.ingredientsData.data})
 
     const handleTitle = (index) => {
-      var text = data[index]?.strDescription || "Desctiption for this ingredient was not found"
-      var seeMore = false
-      const words = text?.split(' ')
-      if(words.length > 100){
-            seeMore = true
-            words.length=50
-            text=words.join(" ") + '...'
-      }
-
-       return [text,seeMore && <Link to="/">See more</Link>]
+         var text = data[index]?.strDescription || "Desctiption for this ingredient was not found"
+         var seeMore = false
+         const words = text?.split(' ')
+         if(words.length > 100){
+               seeMore = true
+               words.length=100
+               text=words.join(" ") + '...'
+         }
+         const handleSeeMore = () => {
+            document.getElementById(`tooltip-${index}`).innerHTML=`${data[index].strDescription}`
+         }
+         return [text,seeMore && <div className={styles.seeMore} onClick={()=>handleSeeMore}>See more</div>]
     }
+
   return (
         <div>
              {drink && data && <div>
@@ -51,7 +57,7 @@ export default function Ingredients({drink}) {
                   {
                   Object.keys(drink).filter((el)=>el.includes("strIngredient") && drink[el]).map((el,index)=>
                      {
-                           return <HtmlTooltip placement="left" key={el} title={<p id='index'>{handleTitle(index)}</p>}>
+                           return <HtmlTooltip placement="left" key={el} title={<p id={`tooltip-${index}`}>{handleTitle(index)}</p>}>
                                        <h1 className={styles.steps} >{drink[el]} - {drink[`strMeasure${index+1}`]}</h1>
                                  </HtmlTooltip>
 
